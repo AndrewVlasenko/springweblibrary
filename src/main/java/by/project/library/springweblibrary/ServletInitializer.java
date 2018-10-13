e1ca7ee58f7157358c43e33f39f99c4d8434ed6d
Andrew Vlasenko <andrewvlasx@gmail.com>
2018-10-13 20:44:25 +0300

    Added message handler

--- src/main/java/by/project/library/springweblibrary/ServletInitializer.java
+++ src/main/java/by/project/library/springweblibrary/ServletInitializer.java
@@ -2,8 +2,10 @@ package by.project.library.springweblibrary;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(scanBasePackages = "by.project.library.springweblibrary")
+@EnableAspectJAutoProxy
 public class ServletInitializer extends SpringBootServletInitializer {
 
 }
